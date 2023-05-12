a5f08ed6cf8bc011eb643d019c1ea55219555145
Samuel Mutemi <mtsammy40@gmail.com>
2023-05-12 20:25:28 +0200

    Add postgres to scripts

--- 161.97.150.144/shut_down_containers.sh
+++ 161.97.150.144/shut_down_containers.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# redis start
+# redis down
 docker stop redis && docker rm redis
-
+# kafka down
 docker-compose -f kafka.yml down
+# postgres down
+docker-compose -f postgres.yml down
--- 161.97.150.144/start_up_containers.sh
+++ 161.97.150.144/start_up_containers.sh
@@ -2,5 +2,7 @@
 
 # redis start
 docker run --name redis -p 6379:6379 -d redis:latest
-
+# kafka start
 docker-compose -f kafka.yml up  -d
+# postgres start
+docker-compose -f postgres.yml up  -d
